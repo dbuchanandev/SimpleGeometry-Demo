49250647c424c9f0f25b566303df71e0a1aba492
Donavon Buchanan <5569820+donavoncade@users.noreply.github.com>
2020-10-07 02:31:21 -0400

    Initial Commit

---
+++ SimpleGeometry-Demo/SimpleGeometry_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleGeometry_DemoApp.swift
+//  SimpleGeometry-Demo
+//
+//  Created by Donavon Buchanan on 10/7/20.
+//
+
+import SwiftUI
+
+@main
+struct SimpleGeometry_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
